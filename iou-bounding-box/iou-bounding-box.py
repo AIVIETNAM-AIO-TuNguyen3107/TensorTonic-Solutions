ff2eb84f75b88f652e5f5e77750b635ed9d44b1c
AIVIETNAM-AIO-TuNguyen3107 <82113294+AIVIETNAM-AIO-TuNguyen3107@users.noreply.github.com>
2026-06-13 09:34:05 +0700

    Solve iou-bounding-box

--- iou-bounding-box/iou-bounding-box.py
+++ iou-bounding-box/iou-bounding-box.py
@@ -13,5 +13,5 @@ def iou(box_a, box_b):
     area_a = (box_a[2]-box_a[0])*(box_a[3]-box_a[1])
     area_b = (box_b[2]-box_b[0])*(box_b[3]-box_b[1])
 
-    iou = intersect_area / (area_a + area_b - intersect_area)
+    iou = intersect_area / (area_a + area_b - intersect_area) if (area_a + area_b - intersect_area) else 0
     return iou
