284418df05a0589585268280ea5bfe3f37a6a44f
AIVIETNAM-AIO-TuNguyen3107 <82113294+AIVIETNAM-AIO-TuNguyen3107@users.noreply.github.com>
2026-06-13 09:31:53 +0700

    Solve non-maximum-suppression

--- non-maximum-suppression/non-maximum-suppression.py
+++ non-maximum-suppression/non-maximum-suppression.py
@@ -12,23 +12,20 @@ def nms(boxes, scores, iou_threshold):
 
         area_a = (box_a[2]-box_a[0])*(box_a[3]-box_a[1])
         area_b = (box_b[2]-box_b[0])*(box_b[3]-box_b[1])
-        return intersection_area / (area_a + area_b - intersection_area)
+        return intersection_area / (area_a + area_b - intersection_area) if (area_a + area_b - intersection_area) else 0
     def get_index_sorted_score(scores):
-        dict_scores = {i: score for i, score in enumerate(scores)}
-        sorted_score = sorted(dict_scores.items(), key=lambda item: item[1], reverse=True)
-        return [index for index, score in sorted_score]
+        indices = sorted(range(len(scores)), key=lambda i: scores[i], reverse=True)
+        return indices
     result = []
-    surpressed_index = set()
 
     index_sorted_score = get_index_sorted_score(scores)
-    for max_index in index_sorted_score:
-        if max_index in surpressed_index:
-            continue
-        result.append(max_index)
-        for i, box in enumerate(boxes):
-            if i == max_index or i in surpressed_index:
-                continue
-            iou_score = iou(box, boxes[max_index])
-            if iou_score >= iou_threshold:
-                surpressed_index.add(i)
+    while index_sorted_score:
+        current_max_index = index_sorted_score.pop(0)
+        result.append(current_max_index)
+        remaining_index = []
+        for i in index_sorted_score:
+            iou_score = iou(boxes[i], boxes[current_max_index])
+            if iou_score < iou_threshold:
+                remaining_index.append(i)
+        index_sorted_score = remaining_index
     return result
