ea8520e0f2c02760da1e539eccf696224e69c004
AIVIETNAM-AIO-TuNguyen3107 <82113294+AIVIETNAM-AIO-TuNguyen3107@users.noreply.github.com>
2026-06-13 09:14:59 +0700

    Solve non-maximum-suppression

---
+++ non-maximum-suppression/non-maximum-suppression.py
@@ -0,0 +1,31 @@
+def nms(boxes, scores, iou_threshold):
+    """
+    Apply Non-Maximum Suppression.
+    """
+    def iou(box_a, box_b):
+        inter_x1 = max(box_a[0], box_b[0])
+        inter_y1 = max(box_a[1], box_b[1])
+        inter_x2 = min(box_a[2], box_b[2])
+        inter_y2 = min(box_a[3], box_b[3])
+
+        intersection_area = max(0, inter_x2 - inter_x1) * max(0, inter_y2 - inter_y1)
+
+        area_a = (box_a[2]-box_a[0])*(box_a[3]-box_a[1])
+        area_b = (box_b[2]-box_b[0])*(box_b[3]-box_b[1])
+        return intersection_area / (area_a + area_b - intersection_area)
+    result = []
+    surpressed_index = set()
+
+    dict_scores = {i: score for i, score in enumerate(scores)}
+    sorted_score = sorted(dict_scores.items(), key=lambda item: item[1], reverse=True)
+    for max_index, _ in sorted_score:
+        if max_index in surpressed_index:
+                continue
+        result.append(max_index)
+        for i, box in enumerate(boxes):
+            if i == max_index:
+                continue
+            iou_score = iou(box, boxes[max_index])
+            if iou_score >= iou_threshold:
+                surpressed_index.add(i)
+    return result
