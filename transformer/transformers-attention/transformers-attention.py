48283f3774d6f3e56aaf847ae26a13f3641e41c8
AIVIETNAM-AIO-TuNguyen3107 <82113294+AIVIETNAM-AIO-TuNguyen3107@users.noreply.github.com>
2026-06-16 14:49:53 +0700

    Solve transformers-attention

--- transformer/transformers-attention/transformers-attention.py
+++ transformer/transformers-attention/transformers-attention.py
@@ -8,7 +8,11 @@ def scaled_dot_product_attention(Q: torch.Tensor, K: torch.Tensor, V: torch.Tens
 
     Attention(Q, K, V) = softmax(Q @ K^T / sqrt(d_k)) @ V
     """
-    d_k = K.size(-1)
+    # with this problem, the d_k equal Q last dimension.
+    # In multi-head attention, Q last dimension usually equal d_model then split into heads d_model = d_k * heads
+    batch, seq_length, d_k = Q.shape
+    # scaled dot-product, feature correlations, not token scores
     scores = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(d_k)
-    attn_weights = F.softmax(scores, dim=-1) # loop through each col in a row
-    return torch.matmul(attn_weights, V)
+    # softmax row-wise, over the key dimension of the last axis
+    attention = torch.matmul(F.softmax(scores, dim=-1), V)
+    return attention
