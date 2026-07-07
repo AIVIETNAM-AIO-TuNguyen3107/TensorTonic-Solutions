b94d76214c2c3be89592f9f15be2a9c9713595ef
AIVIETNAM-AIO-TuNguyen3107 <82113294+AIVIETNAM-AIO-TuNguyen3107@users.noreply.github.com>
2026-07-07 22:35:38 +0700

    Solve transformers-positional-encoding

--- transformer/transformers-positional-encoding/transformers-positional-encoding.py
+++ transformer/transformers-positional-encoding/transformers-positional-encoding.py
@@ -4,12 +4,10 @@ def positional_encoding(seq_length: int, d_model: int) -> np.ndarray:
     """
     Generate sinusoidal positional encodings.
     """
-    pos = np.arange(seq_length).reshape(seq_length, -1)
-    i = np.arange(d_model).reshape(-1, d_model)
-    pair_index = i // 2
-    divider = 10000 ** (2*pair_index/d_model)
+    pair_index = np.arange(0, d_model)
+    position = np.arange(0, seq_length)[:, None]
     return np.where(
-        i % 2 == 0,
-        np.sin(pos/divider),
-        np.cos(pos/divider)
+        pair_index % 2 == 0,
+        np.sin(position / 10000 ** (2*(pair_index//2)/d_model)),
+        np.cos(position / 10000 ** (2*(pair_index//2)/d_model))
     )
