505f8ce6760e3c6595d65f7db13a8a5ba1660211
AIVIETNAM-AIO-TuNguyen3107 <82113294+AIVIETNAM-AIO-TuNguyen3107@users.noreply.github.com>
2026-07-28 21:10:49 +0700

    Solve la-vector-norms

--- study-plans/math-linear-algebra/la-vector-norms/la-vector-norms.py
+++ study-plans/math-linear-algebra/la-vector-norms/la-vector-norms.py
@@ -4,8 +4,7 @@ def vector_norms(v):
     """
     Returns: float64 array of shape (3,) containing [L1, L2, L-inf] norms.
     """
-    v = np.array(v, dtype=np.float64)
-
+    v = np.array(v)
     return np.array(
         [
             np.sum(np.abs(v)),
