d248fbe5d29e1de487f4f51fecf3d7e731254cc4
AIVIETNAM-AIO-TuNguyen3107 <82113294+AIVIETNAM-AIO-TuNguyen3107@users.noreply.github.com>
2026-07-28 21:24:00 +0700

    Solve la-cosine-similarity

--- study-plans/math-linear-algebra/la-cosine-similarity/la-cosine-similarity.py
+++ study-plans/math-linear-algebra/la-cosine-similarity/la-cosine-similarity.py
@@ -6,8 +6,9 @@ def cosine_similarity(a, b):
     """
     a = np.array(a)
     b = np.array(b)
-    length_a = np.sqrt(np.sum(a**2))
-    length_b = np.sqrt(np.sum(b**2))
-    if length_a==0 or length_b==0:
+
+    norm_a = np.sqrt(np.sum(a**2))
+    norm_b = np.sqrt(np.sum(b**2))
+    if norm_a == 0 or norm_b == 0:
         return 0
-    return (a@b)/ (length_a*length_b)
+    return (a @ b.T) / (norm_a * norm_b)
