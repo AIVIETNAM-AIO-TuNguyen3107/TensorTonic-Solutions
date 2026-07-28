73f9275e239539e9e7a942d26614c00f33e4f731
AIVIETNAM-AIO-TuNguyen3107 <82113294+AIVIETNAM-AIO-TuNguyen3107@users.noreply.github.com>
2026-07-28 22:43:43 +0700

    Solve ml-knn-classifier

--- study-plans/cracking-ml/ml-knn-classifier/ml-knn-classifier.py
+++ study-plans/cracking-ml/ml-knn-classifier/ml-knn-classifier.py
@@ -1,23 +1,27 @@
 import numpy as np
-from collections import Counter, defaultdict
+from collections import Counter
 def knn_classify(X_train, y_train, X_test, k=3):
     """
     Returns: A list of predicted integer labels for each test point
     """
     X_train = np.array(X_train)
-    X_test = np.array(X_test)
     y_train = np.array(y_train)
-    dist = np.sqrt(np.sum((X_test[:, np.newaxis,:] - X_train)**2, axis=-1))
-    top_k_indices = np.argsort(dist, axis=-1)[:, :k]
+    X_test = np.array(X_test)
+
+    l2_from_test_to_each_train = np.sqrt(np.sum((X_test[:, np.newaxis, :] - X_train[np.newaxis, :, :])**2, axis=-1))
+
+    sorted_idx_for_each_test = np.argsort(l2_from_test_to_each_train, axis=-1)
+
     result = []
-    for top_k in top_k_indices:
-        counter = Counter(y_train[top_k])
-        is_tie = len(set(counter.values())) != len(counter.values())
+    for sorted_idx in sorted_idx_for_each_test:
+        candidate_labels = y_train[sorted_idx[:k]]
+        unique_labels = set(candidate_labels)
+
+        counter = Counter(candidate_labels)
+        most_common = counter.most_common()
+        is_tie = len([common for common in most_common if common[1] == most_common[0][1]]) > 1
         if is_tie:
-            counter = defaultdict(float)
-            for k_index in top_k:
-                counter[y_train[k_index]] += 1 / dist[:, k_index][0]
-            result.append(sorted(counter)[0])
+            result.append(min(unique_labels))
         else:
-            result.append(counter.most_common()[0][0])
+            result.append(most_common[0][0])
     return result
