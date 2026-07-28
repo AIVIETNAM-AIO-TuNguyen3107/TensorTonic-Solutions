6a1af10fc3cc796bb834464cf6026890631fa996
AIVIETNAM-AIO-TuNguyen3107 <82113294+AIVIETNAM-AIO-TuNguyen3107@users.noreply.github.com>
2026-07-28 21:15:20 +0700

    Solve la-matrix-vector-multiply

--- study-plans/math-linear-algebra/la-matrix-vector-multiply/la-matrix-vector-multiply.py
+++ study-plans/math-linear-algebra/la-matrix-vector-multiply/la-matrix-vector-multiply.py
@@ -4,4 +4,6 @@ def matrix_vector_multiply(A, x):
     """
     Returns: 1-D float64 array, the product A @ x.
     """
-    return np.array(A) @ np.array(x)
+    A = np.array(A)
+    x = np.array(x)
+    return A @ x
